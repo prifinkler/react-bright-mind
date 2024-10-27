3f908575b496e634f19ffa4f371123ef09933b02
joana <joanabrito216@gmail.com>
2024-10-27 10:11:03 +0000

    Changes

--- src/components/FeedbackPage.tsx
+++ src/components/FeedbackPage.tsx
@@ -1,4 +1,10 @@
 import { Link } from "react-router-dom";
+import {
+  faPuzzlePiece,
+  faBed,
+  faLeaf,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function FeedbackPage() {
   return (
@@ -9,13 +15,26 @@ function FeedbackPage() {
       </div>
       <h2 style={{ display: "flex" }}>Do you want to Continue?</h2>
       <div className="action-button">
-        <Link to="/activity" style={{background: "var(--cucumber)", color: "var(--blas-blue)", width: "300px"}}>
-            <img src="/images/puzzle.png" />
-            Try Another Puzzle
+        <Link
+          to="/"
+          style={{ background: "var(--cucumber)", color: "var(--blas-blue)" }}
+        >
+          <FontAwesomeIcon icon={faLeaf} className="star-icon" />
+          A new flower in the garden!
         </Link>
-        <Link to="/"style={{background: "var(--acapulco-blue)", color: "white", width: "220px", height: "100px"}}>
-          <img src="/images/patient.png" />
-         Have a Rest
+        <Link
+          to="/"
+          style={{ background: "var(--cucumber)", color: "var(--blas-blue)" }}
+        >
+          <FontAwesomeIcon icon={faPuzzlePiece} className="star-icon" />
+          Try another Puzzle
+        </Link>
+        <Link
+          to="/"
+          style={{ background: "var(--acapulco-blue)", color: "white" }}
+        >
+          <FontAwesomeIcon icon={faBed} className="star-icon" />
+          Have a Rest
         </Link>
       </div>
     </div>
