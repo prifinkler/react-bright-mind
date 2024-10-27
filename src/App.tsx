31455d429c31781b27deddfe6d2e8b405b88e059
Priscila Finkler <prifinkler@gmail.com>
2024-10-27 10:01:27 +0000

    garden gamification page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GrandmaHomepage from './components/GrandmaHomepage';
+import ActivityPage from './components/ActivityPage';
+import SuccessPage from './components/SuccessPage';
+import GardenProgress from './components/GardenProgress';
+
+import './App.css';
 
 function App() {
-  return <GrandmaHomepage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<GrandmaHomepage />} />
+        <Route path="/activity" element={<ActivityPage />} />
+        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/garden" element={<GardenProgress />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
