fc4df7a0e022583832332a94f2958fd387adc470
rafsasinski <rafsasinski@gmail.com>
2024-10-27 09:50:56 +0000

    vite config for external connection, styling changes for Activity and Feedback

--- src/components/FeedbackPage.tsx
+++ src/components/FeedbackPage.tsx
@@ -9,11 +9,11 @@ function FeedbackPage() {
       </div>
       <h2 style={{ display: "flex" }}>Do you want to Continue?</h2>
       <div className="action-button">
-        <Link to="/activity" style={{background: "var(--cucumber)", color: "var(--blas-blue)"}}>
+        <Link to="/activity" style={{background: "var(--cucumber)", color: "var(--blas-blue)", width: "300px"}}>
             <img src="/images/puzzle.png" />
             Try Another Puzzle
         </Link>
-        <Link to="/"style={{background: "var(--acapulco-blue)", color: "white"}}>
+        <Link to="/"style={{background: "var(--acapulco-blue)", color: "white", width: "220px", height: "100px"}}>
           <img src="/images/patient.png" />
          Have a Rest
         </Link>
